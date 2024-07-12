e3aa749e92931e3b0b37de129e87ec387d340cb7
Hussain Intzar <butthussain2@gmail.com>
2024-07-12 22:33:54 +0500

    Time: 18 ms (5.42%) | Memory: 44.6 MB (5.30%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -6,30 +6,19 @@ class Solution {
 
         for(int i = nums2.length-1; i>=0;i--){
             while(!st.isEmpty() && st.peek()<nums2[i]){
-                System.out.println("pop");
                 st.pop();
-                
             }
-            if(st.isEmpty()){
-                 
-                st.push(nums2[i]);
-                System.out.println("Stack Top: "+ st.peek());
+            if(st.isEmpty()){   
+                st.push(nums2[i]);         
                 hm.put(nums2[i],-1);
-                System.out.println("hashed by cond1: "+ hm.get(nums2[i]));
-               
                 continue;
             }
 
-            else if(st.peek()>nums2[i]){      
-                System.out.println("Stack Top: "+ st.peek());       
+            else if(st.peek()>nums2[i]){           
                 hm.put(nums2[i], st.peek());
                 st.push(nums2[i]);
-                System.out.println("hashed by cond2: "+hm.get(nums2[i]));
-                
                 continue;
             }
-           
-            
         }
             for(int i = 0; i<nums1.length; i++){
                 System.out.println(i);
