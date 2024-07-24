21c5e7ec0ff73e02bac81637fab27b8cef33e298
Hussain Intzar <butthussain2@gmail.com>
2024-07-24 07:55:32 +0500

    Time: 1 ms (94.48%) | Memory: 45 MB (91.76%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -4,16 +4,13 @@ class Solution {
         for (int x: nums){
             sum += x;
         }
-        System.out.print(sum);
         int sum2=0;
         for(int i = 0; i < nums.length; i++){
             sum-=nums[i];
              if(sum == sum2){
                 return i;
             }
-            System.out.println("------"+i+"-------");
-            System.out.println(sum);
-            System.out.println(sum2);
+            
             sum2 += nums[i];
             
            
