e265ead0c84edcea450934a119b2c82c16fe37f3
Hussain Intzar <butthussain2@gmail.com>
2024-07-24 09:42:45 +0500

    Time: 80 ms (18.67%) | Memory: 45.7 MB (23.20%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        if(word1.length()!=word2.length()){
+            System.out.println("...");
+            return false;
+        }
+        HashMap<Character,Integer> hm1 = new HashMap<Character,Integer>();
+        HashMap<Character,Integer> hm2 = new HashMap<Character,Integer>();
+        for(int i = 0; i<word1.length(); i++){
+            hm1.put(word1.charAt(i),hm1.getOrDefault(word1.charAt(i),0)+1);
+        }
+        for(int i = 0; i<word2.length(); i++){
+            hm2.put(word2.charAt(i),hm2.getOrDefault(word2.charAt(i),0)+1);
+        }
+        System.out.println(hm1.values()+" | "+hm2.values());
+
+        List<Integer> values1 = new ArrayList<>(hm1.values());
+        List<Integer> values2 = new ArrayList<>(hm2.values());
+        Collections.sort(values1);
+        Collections.sort(values2);
+        return values1.equals(values2) && hm1.keySet().equals(hm2.keySet());          
+   
+}}
