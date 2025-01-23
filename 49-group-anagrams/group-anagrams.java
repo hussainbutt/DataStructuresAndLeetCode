aa2a93a475f6556e40bb3e7115cf7c890d1edeb1
Hussain Intzar <butthussain2@gmail.com>
2025-01-23 15:01:56 +0500

    Time: 20 ms (16.64%) | Memory: 48.3 MB (29.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -13,7 +13,7 @@ class Solution {
             }
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 26; i++) {
-                sb.append("hehe");
+                sb.append(".");
                 sb.append(count[i]);
             }
             String key = sb.toString();
