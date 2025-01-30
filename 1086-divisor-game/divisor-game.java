8f908367674517d5a0598847aedb9dc2370e0954
Hussain Intzar <butthussain2@gmail.com>
2025-01-30 10:56:19 +0500

    Time: 0 ms (100.00%) | Memory: 40.2 MB (90.77%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean divisorGame(int n) {
+       
+        if(n==1) return false;
+        if(n==2) return true;
+        for(int x = 1; x<n ;x++){
+            if(n%x == 0){
+               return !divisorGame(n-x);
+            }
+        }
+        return false;
+    }
+}
