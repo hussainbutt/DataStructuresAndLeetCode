d6ab6317288699bc7827618837fd3f07da2615b7
Hussain Intzar <butthussain2@gmail.com>
2025-06-23 14:36:28 -0700

    Time: 31 ms (5.13%) | Memory: 45.9 MB (6.00%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[] horizontal = new int[9];
+        int[] vertical = new int[9];
+        HashMap<String, HashSet> hm = new HashMap();
+        HashSet<Character> hs = new HashSet<Character>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.' && hs.contains(board[i][j])) {
+                    
+                    return false;
+                }
+                hs.add(board[i][j]);
+                String key = i / 3 + "," + j / 3;
+                hm.putIfAbsent(key, new HashSet());
+                if (hm.get(key).contains(board[i][j]))
+                    return false;
+                if (board[i][j] != '.') {
+                    hm.get(key).add(board[i][j]);
+                    System.out.println("added an element");
+                }
+            }
+        hs.clear();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] != '.' && hs.contains(board[j][i])) {
+                    System.out.println(board[j][i]);
+                    return false;
+                }
+                hs.add(board[j][i]);
+                String key = (i / 3 + "," + j / 3);
+                
+            }
+            hs.clear();
+        }
+        hs.clear();
+        return true;
+    }
+}
