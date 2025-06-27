a363d275c5605767ee9d521558987dbd9549e8aa
Hussain Intzar <butthussain2@gmail.com>
2025-06-27 12:53:16 -0700

    Time: 844 ms (12.09%) | Memory: 52.4 MB (24.61%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -9,15 +9,10 @@ class Solution {
             while(left<right){
                     int sum = nums[i]+nums[left]+nums[right];
                     if(sum==0){
-                        List<Integer> tempList = new ArrayList<Integer>();
-                        tempList.add(nums[i]);
-                        tempList.add(nums[left]);
-                        tempList.add(nums[right]);
-
                         
-                            hs.add(tempList);
-                            left++;
-                           right--;
+                        hs.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        left++;
+                        right--;
                         }
                     else if(sum>0){
                         right--;
