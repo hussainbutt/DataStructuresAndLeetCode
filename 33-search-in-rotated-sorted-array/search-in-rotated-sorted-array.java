93bf4bd6e9b4eb84973a7c20916e6753310a95c6
Hussain Intzar <butthussain2@gmail.com>
2025-08-13 20:05:51 +0500

    Time: 0 ms (100.00%) | Memory: 42.5 MB (19.71%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -9,18 +9,18 @@ class Solution {
         if(low>high) return -1;
         if(nums[mid] == target)
             return mid;
-
-        //handling if left subarray is sorted
         if(nums[low]<=nums[mid]){
             if(target>=nums[low] && target<=nums[mid]){
+                while(nums[mid] == nums[mid-1]) mid--;
                 return helper(low,mid-1,nums,target);
             }else{
                 return helper(mid+1,high,nums,target);
             }
         }
-        //handling if right subarray is sorted
         else if(nums[mid]<=nums[high]){
             if(target>=nums[mid+1] && target <= nums[high]){
+                while(nums[mid] == nums[mid+1]) mid = mid + 1;
+
                 return helper(mid+1,high,nums,target);
             }
             else{
